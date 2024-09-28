fbdef06165c04e31e6b7be500068a71d5586885f
Md.Sumsul Arifin <s.arifinoriginal@gmail.com>
2024-09-28 20:31:15 +0600

    sad
    asd

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+    {origin: 'http://localhost:5173'}
+));
 app.use(express.json());
 
 
